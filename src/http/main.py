75b06c823e131a2870e2cc9c3352b19017de9e25
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-06-03 14:14:44 +0300

    Make port available for heroku

--- src/http/main.py
+++ src/http/main.py
@@ -1,21 +1,24 @@
 import os
 import http.server as server
 
+PORT = os.environ['PORT']
+# PORT = 8000
+
 
 class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
     """Extend SimpleHTTPRequestHandler to handle PUT requests"""
 
     def do_PUT(self):
         """Save a file following a HTTP PUT request"""
-        filename = os.path.basename(self.path)
+        filename = os.path.basename('image.jpeg')
 
-        # Don't overwrite files
-        if os.path.exists(filename):
-            self.send_response(409, 'Conflict')
-            self.end_headers()
-            reply_body = '"%s" already exists\n' % filename
-            self.wfile.write(reply_body.encode('utf-8'))
-            return
+        # # Don't overwrite files
+        # if os.path.exists(filename):
+        #     self.send_response(409, 'Conflict')
+        #     self.end_headers()
+        #     reply_body = '"%s" already exists\n' % filename
+        #     self.wfile.write(reply_body.encode('utf-8'))
+        #     return
 
         file_length = int(self.headers['Content-Length'])
         with open(filename, 'wb') as output_file:
@@ -27,5 +30,15 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
         self.wfile.write(reply_body.encode('utf-8'))
 
 
+def run(server_class=server.HTTPServer, handler_class=HTTPRequestHandler):
+    server_address = ('', PORT)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
 if __name__ == '__main__':
-    server.test(HandlerClass=HTTPRequestHandler)
+    # server.test(HandlerClass=HTTPRequestHandler)
+    run()
+
+
+
